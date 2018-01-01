432290459b0ac4512a41c46998e0165de44212f2
Christopher Dorst <chris@dorst.email>
2018-01-01 04:03:37 -0600

    Setting container permissions using SetPermissions api

--- DevOps.ContentDelivery.Functions.GetOrCreateAzureBlobContainer/ContainerCreator.cs
+++ DevOps.ContentDelivery.Functions.GetOrCreateAzureBlobContainer/ContainerCreator.cs
@@ -6,9 +6,10 @@ namespace DevOps.ContentDelivery.Functions.GetOrCreateAzureBlobContainer
     internal static class ContainerCreator
     {
         public static async Task EnsureCreated(CloudBlobContainer container)
-            => await container.CreateIfNotExistsAsync(
-                BlobContainerPublicAccessType.Blob,
-                null,
-                null);
+        {
+            await container.CreateIfNotExistsAsync();
+            await container.SetPermissionsAsync(
+                new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+        }
     }
 }
